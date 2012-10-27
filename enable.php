3b817e259242e5fe04e8b7e12b0e13df490df989
marekmurawski <marek.murawski@gmail.com>
2012-10-27 15:45:21 +0200

    testing

--- enable.php
+++ enable.php
@@ -63,6 +63,35 @@ function copyr($source, $dest) {
     return true;
 }
 
+
+$data = '<?php
+
+require_once "slirconfigdefaults.class.php";
+
+class SLIRConfig extends SLIRConfigDefaults
+{
+	public static $errorImages	= FALSE;
+	public static $documentRoot	= "'.CMS_ROOT.'";
+	public static $SLIRDir	= "/thmm";
+	public static $cacheDirName	= "/thmm/cache";
+	public static $cacheDir	= "'.CMS_ROOT.'/thmm/cache";
+    
+	public static function init()
+	{
+		// This must be the last line of this function
+		parent::init();
+	}
+}
+
+SLIRConfig::init();
+';
+
+file_put_contents(CMS_ROOT . PLUGINS_ROOT . '/mm_thumbs/lib/thmm/slirconfig.class.php', $data);
+
+
+
+
+
 $srcDir = PLUGINS_ROOT . '/mm_thumbs/lib/thmm';
 $destDir = CMS_ROOT . '/thmm';
 
@@ -83,70 +112,5 @@ Deny from All
 
 file_put_contents(CMS_ROOT . '/thmm/.htaccess', $data);
 
-
-$data = '<?php
-
-require_once \'slirconfigdefaults.class.php\';
-
-class SLIRConfig extends SLIRConfigDefaults
-{
-	/**
-	 * Whether SLIR should generate and output images from error messages
-	 * 
-	 * @since 2.0
-	 * @var boolean
-	 */
-	public static $errorImages	= TRUE;
-
-	/**
-	 * Absolute path to the web root (location of files when visiting
-	 * http://domainname.com/) (no trailing slash)
-	 * 
-	 * @since 2.0
-	 * @var string
-	 */
-	public static $documentRoot	= "'.CMS_ROOT.'";
-
-	/**
-	 * Path to SLIR (no trailing slash)
-	 * 
-	 * @since 2.0
-	 * @var string
-	 */
-	//public static $SLIRDir	= \'/thmm\' ;
-
-	/**
-	 * Name of directory to store cached files in (no trailing slash)
-	 * 
-	 * @since 2.0
-	 * @var string
-	 */
-	//public static $cacheDirName	= \'/thmm/cache\';
-
-	/**
-	 * Absolute path to cache directory. This directory must be world-readable,
-	 * writable by the web server, and must end with SLIR_CACHE_DIR_NAME (no
-	 * trailing slash). Ideally, this should be located outside of the web tree.
-	 * 
-	 * @var string
-	 */
-	public static $cacheDir	= "'.CMS_ROOT.'/thmm/cache";
-    
-
-
-	public static $defaultCropper	= SLIR::CROP_CLASS_CENTERED;
-    
-	public static function init()
-	{
-		// This must be the last line of this function
-		parent::init();
-	}
-}
-
-SLIRConfig::init();
-';
-
-
-file_put_contents(CMS_ROOT . '/thmm/slirconfig.class.php', $data);
 
 exit();
--- views/settings.php
+++ views/settings.php
@@ -5,42 +5,81 @@ if (!defined('IN_CMS')) { exit(); }
 ?>
 <h1><?php echo __('mmThumbs'); ?></h1>
 <h2>Requirements</h2>
+<div style="background-color: #DDD; padding: 16px; border: 1px solid #888;">
 <ul style="color: #888">
 <?php
  if (function_exists('imageconvolution')) {
-    echo '<li>GD Library - <span style="color: green">GD library detected</span></li>';
+    echo '<li>GD Library:<br/><span style="margin-left:32px; color: green">GD library detected</span></li>';
   } else {
-    echo '<li>GD Library - <span style="color: red">GD library not found!</b></li>';
+    echo '<li>GD Library:<br/><span style="margin-left:32px; color: red">GD library not found!</b></li>';
   }
   if (version_compare(PHP_VERSION, '5.1.0') >= 0) {
-    echo '<li>PHP Version - <span style="color: green">PHP version is higher than 5.1.0, your version: ' . PHP_VERSION . '</span></li>';
+    echo '<li>PHP Version:<br/><span style="margin-left:32px; color: green">PHP version is higher than 5.1.0, your version: ' . PHP_VERSION . '</span></li>';
   }  else {
-    echo '<li>PHP Version - <span style="color: red">PHP version lower than 5.1.0, your version: ' . PHP_VERSION . '</span></li>';
+    echo '<li>PHP Version:<br/><span style="margin-left:32px; color: red">PHP version lower than 5.1.0, your version: ' . PHP_VERSION . '</span></li>';
   }
  if (USE_MOD_REWRITE) {
-    echo '<li>Use MOD_REWRITE - <span style="color: green">MOD_REWRITE is turned ON</span></li>';
+    echo '<li>Use MOD_REWRITE:<br/><span style="margin-left:32px; color: green">MOD_REWRITE is turned ON</span></li>';
   } else {
-    echo '<li>Use MOD_REWRITE - <span style="color: red">MOD_REWRITE is turned OFF</span></li>';
+    echo '<li>Use MOD_REWRITE:<br/><span style="margin-left:32px; color: red">MOD_REWRITE is turned OFF</span></li>';
   }
  if (file_exists(CMS_ROOT.'/.htaccess')) {
-    echo '<li>/.htaccess exists - <span style="color: green">'.CMS_ROOT.'/.htaccess file found! URL rewriting seems to work</span></li>';
+    echo '<li>[CMS_ROOT]/.htaccess exists:<br/><span style="margin-left:32px; color: green">'.CMS_ROOT.'/.htaccess file found! URL rewriting seems to work</span></li>';
   } else {
-    echo '<li>/.htaccess exists - <span style="color: red">'.CMS_ROOT.'/.htaccess file not found! Setup your URL rewriting!</span></li>';
+    echo '<li>[CMS_ROOT]/.htaccess exists:<br/><span style="margin-left:32px; color: red">'.CMS_ROOT.'/.htaccess file not found! Setup your URL rewriting!</span></li>';
   }
  if (file_exists(CMS_ROOT.'/thmm/.htaccess')) {
-    echo '<li>/thmm/.htaccess exists - <span style="color: green">'.CMS_ROOT.'/thmm/.htaccess file found!</span></li>';
+    echo '<li>[CMS_ROOT]/thmm/.htaccess exists:<br/><span style="margin-left:32px; color: green">'.CMS_ROOT.'/thmm/.htaccess file found!</span></li>';
   } else {
-    echo '<li>/thmm/.htaccess exists - <span style="color: red">'.CMS_ROOT.'/thmm/.htaccess file not found! Try re-enabling the plugin</span></li>';
+    echo '<li>[CMS_ROOT]/thmm/.htaccess exists:<br/><span style="margin-left:32px; color: red">'.CMS_ROOT.'/thmm/.htaccess file not found! Try re-enabling the plugin</span></li>';
   }
  if (file_exists(CMS_ROOT.'/thmm/index.php')) {
-    echo '<li>/thmm/index.php exists - <span style="color: green">'.CMS_ROOT.'/thmm/index.php file found!</span></li>';
+    echo '<li>[CMS_ROOT]/thmm/index.php exists:<br/><span style="margin-left:32px; color: green">'.CMS_ROOT.'/thmm/index.php file found!</span></li>';
   } else {
-    echo '<li>/thmm/index.php exists - <span style="color: red">'.CMS_ROOT.'/thmm/index.php file not found! Try re-enabling the plugin</span></li>';
+    echo '<li>[CMS_ROOT]/thmm/index.php exists:<br/><span style="margin-left:32px; color: red">'.CMS_ROOT.'/thmm/index.php file not found! Try re-enabling the plugin</span></li>';
   }
   
   
 ?>
 </ul>
+</div>
+
+<div style="background-color: #FDB; padding: 16px; margin-left: 32px; margin-top: 16px;">
+  <h3>Installation troubleshooting</h3>
+<p>
+  Depending on your file permission settings it can be impossible for plugin to create <b>[CMS_ROOT]/thmm</b> directory and copy neccessary files into it.
+</p>
+<p>
+  Try to manually copy this directory:<br/>
+  <b><span style="color: blue;"><?php echo PLUGINS_ROOT; ?></span>/mm_thumbs/lib/thmm</b> directory on your server <br/>
+  to <br/>
+  <b><span style="color: blue;"><?php echo CMS_ROOT; ?></span>/thmm</b> directory (side by side with <b>/wolf</b> directory)
+</p>
+<p>And then re-enable the plugin in Administration panel of Wolf CMS</p>
+</div>
+
+<div style="background-color: #FDB; padding: 16px; margin-left: 32px; margin-top: 16px;">
+  <h3>Special URL rewriting case</h3>
+<p>
+  If you're using a method of URL rewriting of Wolf CMS residing in subdirectory, for example in <b>.../public_html/wolfcms/</b> <br/>
+  <b>http://www.yoursite.com/wolfcms/</b> subdirectory, 
+  <br/>but publicly it is accessible via: <br/>
+  <b>http://www.yoursite.com/</b> <br/><br/>
+  <i>This method is described in this forum topic <a href="http://www.wolfcms.org/forum/topic307.html">http://www.wolfcms.org/forum/topic307.html</a></i>
+</p>
+<p>
+  You need to edit your <b>.../public_html/.htaccess</b> <i>(the DOMAIN ROOT .htaccess, not the one in Wolf root dir)</i> like this:
+</p>
+<pre>
+  RewriteEngine On
+
+  # the following line should be inserted 
+  # just after RewriteEngine On statement
+
+  RewriteRule ^thmm(.*)$ wolfcms/thmm/index.php$1 [L]
+</pre>
+<p>and then <b>DELETE .../public_html/wolfcms/thmm/.htaccess</b> or RENAME it to for example <b>_.htaccess</b></p>
+</div>
 <h2>Usage</h2>
 <p>
   This plugin installs SLIR script in ROOT directory of your Wolf CMS installation. 
@@ -54,7 +93,7 @@ if (!defined('IN_CMS')) { exit(); }
   Or with short URL starting with slash:
 </p>
 <p>
-  <b>/<span style="color: blue">thmm</span>/</b><span style="color: red">parameters</span><span style="color: green">/path/to/your/image.jpg</span>
+  <b><?php echo URI_PUBLIC; ?><span style="color: blue">thmm</span>/</b><span style="color: red">parameters</span><span style="color: green">/path/to/your/image.jpg</span>
 </p>
 <pre>
    To use, place an img tag with the src pointing to the path of "/thmm/"
@@ -81,12 +120,7 @@ if (!defined('IN_CMS')) { exit(); }
 <p>If you want to turn it off, edit <b>mm_thumbs/index.php</b> and set </b>$integrate_with_file_manager = false;</b></p>
 <h2>Playground area</h2>
 <p>Try out typing parameters for this sample image (800x600px) to see live effects of various parameters. You can also click "Randomize!" to create random set of parameters.</p>
-<p style="font-weight: bold;">
-  <?php echo CMS_ROOT; ?>
-</p>
-<p style="font-weight: bold;">
-  <?php echo URI_PUBLIC; ?>
-</p>
+
 <p style="font-weight: bold;">
   &lt;img src="<?php echo URI_PUBLIC; ?>thmm/<input id="params" value="w80"><?php echo '/wolf/plugins/mm_thumbs/images/sample.jpg' ?>" /&gt;
 </p>
@@ -94,7 +128,11 @@ if (!defined('IN_CMS')) { exit(); }
   <input type="button" id="resetImage" value="Reset Image">
   <input type="button" id="randomParams" value="Randomize!">
 </p>
-<hr/>
+
+<div style="background-color: #FDB; padding: 8px; margin-top: 8px; margin-bottom: 8px;">
+Sample image URI: <span id="imageuri" style="font-weight: bold; border: 1px solid red"></span>
+</div>
+
 <img id="sample" src="/thmm/w80/wolf/plugins/mm_thumbs/images/sample.jpg">
 
 <script type="text/javascript">
@@ -130,8 +168,10 @@ if (!defined('IN_CMS')) { exit(); }
     
     $(document).ready(function() {
         $('#params').keyup(function() { 
-          var newUri = '/thmm/' + $('#params').val() + '<?php echo URI_PUBLIC . 'wolf/plugins/mm_thumbs/images/sample.jpg' ?>';
+          var newUri = '<?php echo URI_PUBLIC; ?>' + 'thmm/' + $('#params').val() + '<?php echo '/wolf/plugins/mm_thumbs/images/sample.jpg' ?>';
           $('#sample').attr('src', newUri); 
+          $('#imageuri').html(newUri); 
+          
         });
 
         $('#resetImage').click(function() { 
@@ -145,6 +185,7 @@ if (!defined('IN_CMS')) { exit(); }
         });
         
         $('#resetImage').trigger('click');
+        $('#params').trigger('keyup');
     });
 // ]]>
 </script>
