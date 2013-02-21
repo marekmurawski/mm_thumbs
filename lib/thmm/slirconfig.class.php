8b3b6994daee9fe895aed7559c8cc2d8ca59efe7
marekmurawski <marek.murawski@gmail.com>
2013-02-21 11:20:40 +0100

    dev

--- lib/thmm/slirconfig.class.php
+++ lib/thmm/slirconfig.class.php
@@ -4,7 +4,17 @@ require_once "slirconfigdefaults.class.php";
 
 class SLIRConfig extends SLIRConfigDefaults
 {
-
+	public static $errorImages	= FALSE;
+	public static $documentRoot	= "/home2/murekpro/public_html/marekmurawski.pl";
+	public static $SLIRDir	= "/thmm";
+	public static $cacheDirName	= "/thmm/cache";
+	public static $cacheDir	= "/home2/murekpro/public_html/marekmurawski.pl/thmm/cache";
+    
+	public static function init()
+	{
+		// This must be the last line of this function
+		parent::init();
+	}
 }
 
 SLIRConfig::init();
