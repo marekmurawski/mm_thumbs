d7ffcb817d372de0fc108ad9bee0796779c6d275
marekmurawski <marek.murawski@gmail.com>
2013-02-26 21:42:39 +0100

    dev

--- MmThumbsController.php
+++ MmThumbsController.php
@@ -1,41 +1,54 @@
 <?php
 
 /* Security measure */
-if (!defined('IN_CMS')) { exit(); }
+if ( !defined( 'IN_CMS' ) ) {
+    exit();
+}
+
 
 class MmThumbsController extends PluginController {
 
     public function __construct() {
-        $this->setLayout('backend');
-        $this->assignToLayout('sidebar', new View('../../plugins/mm_thumbs/views/sidebar'));
+        $this->setLayout( 'backend' );
+        $this->assignToLayout( 'sidebar', new View( '../../plugins/mm_thumbs/views/sidebar' ) );
+
     }
+
 
     public function index() {
         $this->settings();
+
     }
+
 
     function settings() {
-        $requestDir     = CMS_ROOT.'/thmm/cache/request/';
-        $renderDir      = CMS_ROOT.'/thmm/cache/rendered/';
-        
-        $dir = opendir($requestDir);
-        $i = 0;
-        while (false !== ($file = readdir($dir))){
-            if (!in_array($file, array('.', '..')) and !is_dir($file)) $i++;
-        }
-        $requestCount = $i;
-        
-        $dir = opendir($renderDir);
-        $i = 0;
-        while (false !== ($file = readdir($dir))){
-            if (!in_array($file, array('.', '..')) and !is_dir($file)) $i++;
-        }
-        $renderCount = $i;
+        $requestDir = CMS_ROOT . DS . 'thmm' . DS . 'cache' . DS . 'request' . DS;
+        $renderDir  = CMS_ROOT . DS . 'thmm' . DS . 'cache' . DS . 'rendered' . DS;
 
-        $this->display('mm_thumbs/views/settings', array(
-                                'settings'          => Plugin::getAllSettings('mmthumbs'),
-                                'renderCount'       => $renderCount,
-                                'requestCount'      => $requestCount,
-                ));
+        if ( $dir = opendir( $requestDir ) ) {
+            $i    = 0;
+            while ( false !== ($file = readdir( $dir )) ) {
+                if ( !in_array( $file, array( '.', '..' ) ) and !is_dir( $file ) )
+                    $i++;
+            }
+            $requestCount = $i;
+        }
+
+        if ( $dir = opendir( $renderDir ) ) {
+            $i    = 0;
+            while ( false !== ($file = readdir( $dir )) ) {
+                if ( !in_array( $file, array( '.', '..' ) ) and !is_dir( $file ) )
+                    $i++;
+            }
+            $renderCount = $i;
+        }
+        $this->display( 'mm_thumbs/views/settings', array(
+                    'settings'     => Plugin::getAllSettings( 'mmthumbs' ),
+                    'renderCount'  => $renderCount,
+                    'requestCount' => $requestCount,
+        ) );
+
     }
+
+
 }
